eb82960c8608539d949e3dd20f58e70039e2488a
saulivor <saulivorfo@gmail.com>
2023-07-14 18:48:29 +0700

    created two_sum and implemented remove_nonpositive_edges in max_subarray

--- practice_problems/leetcode/max_subarray.jl
+++ practice_problems/leetcode/max_subarray.jl
@@ -1,7 +1,23 @@
 """ 
 max_subarray(nums::Array{Real})::Array{Real}
 
-returns the subarray of nums with the largest sum 
+return subarray of nums with the largest sum 
 
 e.g. max_subarray([-2,1,-3,4,-1,2,1,-5,4]) ->[4,-1,2,1]
 """
+function max_subarray(nums::Array{<:Real})::Array{<:Real}
+end
+
+
+"""
+`remove_nonpositive_edges(nums::Array{<:Real})::Array{<:Real}`
+
+remove nonpositive values from the start and end of the array
+
+e.g. `remove_nonpositive_edges([-1,-2,7,-3,8,12,-5]) -> [7,-3,8,12]`
+"""
+function remove_nonpositive_edges(nums::Array{<:Real})
+    start = findfirst(x -> x>0, nums)
+    finish = findlast(x -> x>0, nums)
+    return nums[start:finish]
+end
