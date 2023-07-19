2430fa71d47d8665d038acfb10fdb758b56f1974
saulivor <saulivorfo@gmail.com>
2023-07-19 15:13:44 +0700

    started core function
    realised the entire program can be encapsulated in this function, and so the code will need refactoring once the function is written

--- practice_problems/leetcode/max_subarray.jl
+++ practice_problems/leetcode/max_subarray.jl
@@ -10,8 +10,7 @@ function max_subarray(nums::Array{<:Real})::Array{<:Real}
     threshold = maximum(nums)
     max_subarray = nums
     while !finished
-        remove_nonpositive_edges(max_subarray)
-        
+                
 
 end
 
@@ -28,3 +27,29 @@ function remove_nonpositive_edges!(nums::Array{<:Real})
     finish = findlast(x -> x>0, nums)
     nums = nums[start:finish]
 end
+
+
+"""
+`remove_nonpositive_edge_subarrays!(nums::Array{<:Real}, threshold::Real)`
+
+
+remove nonpositive subarrays from start and end of the array
+"""
+function remove_nonpositive_edge_subarrays!(nums::Array{<:Real}, threshold::Real, dir = 1)
+    finished = false 
+    subtotal = 0
+    index = dir == 1 ? 1 : length(nums) # sets index to correct end of array depending on iteration direction
+    while !finished 
+        subtotal += nums[index]
+        index += dir
+        if subtotal ≤ 0
+            nums = dir == 1 ? nums[index:end] : nums[1:index] # removes negative subarray from array
+        elseif subtotal > threshold
+            threshold = subtotal
+
+
+
+# this function ^ can probably do the entire computation, iterating inwards from both sides and removing subarrays
+# smaller than the `threshold` (current largest subarray) and updating this threshold whenever it is beaten
+#  - just need to thing about how to handle then end case, on actually ending the iteration and returning the 
+#  remeaining max subarray
