ff26015e8a7af5afbaef1d480830359d3b0b0fc3
saulivor <saulivorfo@gmail.com>
2023-08-21 10:34:22 +0200

    solved git add max_subarray.jl
    used much simpler, single function 'running total' iteration to find the largest subarray in one pass - O(n)

--- practice-problems/leetcode/max_subarray.jl
+++ practice-problems/leetcode/max_subarray.jl
@@ -1,55 +1,31 @@
-""" 
-max_subarray(nums::Array{Real})::Array{Real}
+"""
+`max_subarray(nums::Array{<:Real})::Array{<:Real}`
 
-return subarray of nums with the largest sum 
+returns the subarray with the largest sum
 
-e.g. max_subarray([-2,1,-3,4,-1,2,1,-5,4]) ->[4,-1,2,1]
+(will return the first subarray in case of multiple solutions
+also solves for shortest form i.e. `[3-2,8]` rather than `[0,2,-2,8,-1,1]` 
+
+# examples
+
+max_subarray([1,7,-6,8,-5]) => [1,7,-6,8]
 """
 function max_subarray(nums::Array{<:Real})::Array{<:Real}
-    finished = false
-    threshold = maximum(nums)
-    max_subarray = nums
-    while !finished
-                
+    max_subarray_total = nums[1] 
+    max_subarray_position = (1,1) # initialise largest subarray as first index
 
+    start_index = 1
+    running_subarray_total = 0
+    for (index, value) in enumerate(nums)
+        running_subarray_total += value
+        if running_subarray_total ≤ 0
+            start_index = index+1
+            running_subarray_total = 0
+        elseif running_subarray_total > max_subarray_total
+            max_subarray_total = running_subarray_total
+            max_subarray_position = (start_index, index)
+        end
+    end
+    return nums[max_subarray_position[1]:max_subarray_position[2]]
 end
 
-
-"""
-`remove_nonpositive_edges!(nums::Array{<:Real})::Array{<:Real}`
-
-remove nonpositive values from the start and end of the array
-
-e.g. `remove_nonpositive_edges!([-1,-2,7,-3,8,12,-5]) -> [7,-3,8,12]`
-"""
-function remove_nonpositive_edges!(nums::Array{<:Real})
-    start = findfirst(x -> x>0, nums)
-    finish = findlast(x -> x>0, nums)
-    nums = nums[start:finish]
-end
-
-
-"""
-`remove_nonpositive_edge_subarrays!(nums::Array{<:Real}, threshold::Real)`
-
-
-remove nonpositive subarrays from start and end of the array
-"""
-function remove_nonpositive_edge_subarrays!(nums::Array{<:Real}, threshold::Real, dir = 1)
-    finished = false 
-    subtotal = 0
-    index = dir == 1 ? 1 : length(nums) # sets index to correct end of array depending on iteration direction
-    while !finished 
-        subtotal += nums[index]
-        index += dir
-        if subtotal ≤ 0
-            nums = dir == 1 ? nums[index:end] : nums[1:index] # removes negative subarray from array
-        elseif subtotal > threshold
-            threshold = subtotal
-
-
-
-# this function ^ can probably do the entire computation, iterating inwards from both sides and removing subarrays
-# smaller than the `threshold` (current largest subarray) and updating this threshold whenever it is beaten
-#  - just need to thing about how to handle then end case, on actually ending the iteration and returning the 
-#  remeaining max subarray
