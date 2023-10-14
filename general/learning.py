3d02bd61f07926216f8348211dadc815f068d113
QuantuMechanix8 <saulivorfo@gmail.com>
2023-10-14 22:58:33 +0100

    added get_columns and get_rows to sudoku solver

--- general/learning.py
+++ general/learning.py
@@ -1,3 +1,5 @@
+import math
+
 """initialises user inputted sudoku grid as 2d array"""
 def initialise_grid():
     # grid size has to be a square number - so should validate this
@@ -17,4 +19,29 @@ def solve(sudoku_grid):
 
 
 def next_move(sudoku_grid):
-    digits = list(range)
+    grid_size = len(sudoku_grid[0])
+    digits = list(range(grid_size))
+    for digit in digits:
+        pass
+
+
+def get_columns(sudoku_grid):
+    grid_size = len(sudoku_grid[0])
+    return [[sudoku_grid[i][j] for i in range(grid_size)] for j in range(grid_size)]
+
+
+def get_rows(sudoku_grid):
+    grid_size = len(sudoku_grid[0])
+    return [[sudoku_grid[i][j] for j in range(grid_size)] for i in range(grid_size)]
+
+
+def get_squares(sudoku_grid):
+    grid_size = len(sudoku_grid[0])
+    square_size = int(math.sqrt(grid_size))
+
+
+
+sudoku_grid = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
+print(f"sudoku grid is:\n{sudoku_grid}")
+print(get_columns(sudoku_grid))
+#print(get_rows(sudoku_grid))
